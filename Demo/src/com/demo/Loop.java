9a91ee2d4ca6877df8278d7ebbb5ca7c0d6901d4
Dipanshu <Dipanshu@LAPTOP-SIK1VRAL>
2025-12-18 08:27:08 +0530

    Testing different output by having different data types in inputs. like binary , hexadecimal or octa decimal.

---
+++ Demo/src/com/demo/Loop.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 45;
+		System.out.println(a);
+		int b = 045;
+		System.out.println(b);
+		int c = 0x45;
+		System.out.println(c);
+		int d = 0b1011;
+		System.out.println(d);
+
+	}
+
+}
