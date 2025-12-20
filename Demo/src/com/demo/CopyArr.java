b2ce3407e8e67a6a36506222d3bb9cc346da165c
Dipanshu <Dipanshu@192.168.1.12>
2025-12-20 22:25:51 +0530

    Storing Value of Array 1 into Array 2. 1st as ascending order and than in Revered order.

---
+++ Demo/src/com/demo/CopyArr.java
@@ -0,0 +1,31 @@
+package com.demo;
+
+import java.util.Scanner;
+
+public class CopyArr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		 int[] a = new int[5];
+		a[0] = 10;
+		a[1] = 20;
+		a[2] = 30;
+		a[3] = 40;
+		a[4] = 50;
+		
+		int[] b = new int[a.length];
+		int j = a.length-1;
+		for(int i=0;i<a.length;i++) {
+			b[i]=a[j];
+			j--;
+		}
+		
+		for(int i=0;i<b.length;i++) {
+			System.out.print(b[i]+" ");
+		}
+
+	}
+
+}
