a9f2419845aa4eff6e568b043ffefa0b91b2bd01
Dipanshu <Dipanshu@LAPTOP-SIK1VRAL>
2025-12-23 08:41:26 +0530

    bubble sorting in array.

---
+++ Demo/src/com/demo/Bubble_Sort_1.java
@@ -0,0 +1,39 @@
+//package com.demo;
+//
+//import java.util.Scanner;
+//
+//public class Bubble_Sort_1 {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int a[] = {10,80,100,50,40,20,30,90};
+//		System.out.println("After Sorting :-");
+//		for(int i=0;i<a.length;i++) {
+//			System.out.print(a[i]+ " ");
+//		}
+//
+//		
+//		for(int i=0;i<a.length;i++) {
+//			for(int j=0;j<a.length;j++) {
+//				if(a[j]>a[j+1]) {
+//					int temp= a[j];
+//					a[j] = a[j+1];
+//					a[j+1]=temp;
+//				}
+//			}
+//			System.out.println("After Sorting :-");
+//			for(int i=0;i<a.length;i++) {
+//				System.out.print(a[i]+ " ");
+//			}
+//		}
+//		
+//		
+//		
+//		
+//		
+//		sc.close();
+//	}
+//
+//}
